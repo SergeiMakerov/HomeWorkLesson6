972b5bcc916f657708f6b63a32b00c8d3d161f82
Sergei <makerovs@yandex.ru>
2023-02-06 18:54:07 +0700

    add Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -6,6 +6,7 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
+
 Console.WriteLine("введите значение b1");
 double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите значение k1");
@@ -15,7 +16,19 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите значение k2");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (-b2 + b1) / (-k1 + k2);
-double y = k2 * x + b2;
+PrintResult();
 
-Console.WriteLine($"Координаты точки пересечения двух прямых X: {x}, Y: {y}");
+double GetX(double b1, double b2, double k1, double k2)
+{
+    return (-b2 + b1) / (-k1 + k2);
+}
+
+double GetY(double x, double b2, double k2)
+{
+    return k2 * x + b2;
+}
+
+void PrintResult()
+{
+    Console.WriteLine($"Координаты точки пересечения двух прямых X: {GetX(b1, b2, k1, k2)}, Y: {GetY(GetX(b1, b2, k1, k2), b2, k2)})");
+}
